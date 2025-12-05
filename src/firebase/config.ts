49607f59db2ec4c5b79d3b34cfebda567d1537e3
Nevin Alcober <85alcober@gmail.com>
2025-12-05 08:07:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4278348085-cfe4e",
+  "appId": "1:666928731582:web:b486c5b0b39218dab4d5ef",
+  "apiKey": "AIzaSyDL-ae-9pf3uTOmwFKibrYOkjUbaidycI0",
+  "authDomain": "studio-4278348085-cfe4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "666928731582"
+};
